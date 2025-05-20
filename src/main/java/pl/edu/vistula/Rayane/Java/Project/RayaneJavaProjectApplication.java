c0d8b225d84538e17c7e4f364403886ff8c9a539
bouch <rbouche1@stu.vistula.edu.pl>
2025-05-20 13:06:31 +0200

    Final Project

---
+++ src/main/java/pl/edu/vistula/Rayane/Java/Project/RayaneJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.Rayane.Java.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RayaneJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RayaneJavaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/vistula/Rayane/Java/Project/RayaneJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.Rayane.Java.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RayaneJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
